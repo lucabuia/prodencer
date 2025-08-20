68c05bbd4048fe9e30933346b0ff4f3eb33d3341
Luca Buiarelli <126896317+lucabuia@users.noreply.github.com>
2025-08-20 15:37:21 +0200

    Update MnF2.py

--- examples/MnF2/MnF2.py
+++ examples/MnF2/MnF2.py
@@ -8,8 +8,11 @@ from spgrep.representation import get_character
 center_Mn = np.array([0.5, 0.5, 0.5]) # Coordinates of the central Mn ion
 radius_Mn = 2.11192
 
-# # It can also be done for the F ions!
+# It can also be done for the F ions
 # center_F = np.array([0.30464, 0.30464, 0]) # Coordinates of one of the F ions
 # radius_F = 1.40289
 
+# You can either use Abinit
 pd.project_harmonics("MnF2_DEN.nc", "abinit", center_Mn, radius_Mn, 419, output_analytical=False)
+# or vasp
+# pd.project_harmonics("MnF2_CHGCAR", "vasp", center_Mn, radius_Mn, 419, output_analytical=False)
