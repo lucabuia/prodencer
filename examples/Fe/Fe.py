92ba1cb5a18beb280bdcf6500ad03c003799e05e
Luca Buiarelli <126896317+lucabuia@users.noreply.github.com>
2025-09-05 09:01:49 +0200

    Iron

--- examples/Fe/Fe.py
+++ examples/Fe/Fe.py
@@ -4,6 +4,6 @@ input_file = "GSo_DEN.nc"
 dft_code = "abinit"
 
 # Project the charge and spin density around the Fe ion onto the tesseral harmonics
-radius = 1.24
+radius = 1
 
 pd.project_harmonics(input_file, dft_code, [0,0,0], radius)
