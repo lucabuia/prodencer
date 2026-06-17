Fixed units in examples too

examples/CrSb/CrSb.py
@@ -15,7 +15,7 @@ space_group_number = 488 # P6_3/mmc, for full list of Hall numbers check https:/
 
 # Cr ion
 center_Cr = np.array([0, 0, 0.5]) # Coordinates of one Cr ion
-radius_Cr = 2.11 # In units of Bohr
+radius_Cr = 1.05 # In units of Angstroms
 
 # Using Abinit
 pd.project_harmonics("CrSbo_DEN.nc", "abinit", center_Cr, radius_Cr, space_group_number, output_components=False, decimals=5)

examples/KMnF3/KMnF3.py
@@ -15,7 +15,7 @@ space_group_number = 425 # I4/mcm, for full list of Hall numbers check https://y
 
 # Mn ion
 center_Mn = np.array([0.2257, 0.7257, 0.5]) # Coordinates of one Mn ion
-radius_Mn = 2.11 # In units of Bohr
+radius_Mn = 1.05 # In units of Angstrom
 
 # Use Abinit
 pd.project_harmonics("KMnF3_DEN.nc", "abinit", center_Mn, radius_Mn, space_group_number, output_components=False, decimals=7)

examples/MnF2/MnF2.py
@@ -15,11 +15,11 @@ space_group_number = 419 # P4_2/mnm, for full list of Hall numbers check https:/
 
 # Central Mn ion
 center_Mn = np.array([0.5, 0.5, 0.5]) # Coordinates of the central Mn ion
-radius_Mn = 2.11 # In units of Bohr
+radius_Mn = 1.05 # In units of Angstroms
 
 # It can also be done for the F ions
 # center_F = np.array([0.30464, 0.30464, 0]) # Coordinates of one of the F ions
-# radius_F = 1.40
+# radius_F = 0.7
 
 # You can either use Abinit
 pd.project_harmonics("MnF2o_DEN.nc", "abinit", center_Mn, radius_Mn, space_group_number, output_components=False, decimals=5)

examples/MnF2/MnF2_IPvsOP.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Central Mn ion
 center_Mn = np.array([0.5, 0.5, 0.5]) # Coordinates of the central Mn ion
-radius_Mn = 3
+radius_Mn = 1.5
 
 lattice, atomic_positions, atomic_species, grid, charge, mx, my, mz = pd.ABINIT_get_density("MnF2o_DEN.nc")
 